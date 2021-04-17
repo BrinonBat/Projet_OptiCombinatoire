8c366df276209272754caa8353523d415238b316
Smitaty <mitatysimon49@gmail.com>
2021-04-17 15:29:55 +0200

    Ajout stat ratio terrain occupé

--- src/Launcher.java
+++ src/Launcher.java
@@ -9,6 +9,7 @@ public class Launcher {
         int sum=0;
         int square_sum=0;
         int sum_case_vide = 0;
+        int sum_ratio_aire = 0;
         switch(algo){
             case 1:{
                 for (int i = 0; i < nb_rep; i++) {
@@ -17,6 +18,7 @@ public class Launcher {
                     sum+=score;
                     square_sum+= score*score;
                     sum_case_vide += terrain.nbCaseVide(terrain.getTerrain());
+                    sum_ratio_aire += (score*100)/(terrain.getLarg()*terrain.getProf());
                     terrain.terrainVide();
                 }
                 break;
@@ -28,6 +30,7 @@ public class Launcher {
                     sum+=score;
                     square_sum+= score*score;
                     sum_case_vide += terrain.nbCaseVide(terrain.getTerrain());
+                    sum_ratio_aire += (score*100)/(terrain.getLarg()*terrain.getProf());
                     terrain.terrainVide();
                 }
                 break;
@@ -39,6 +42,7 @@ public class Launcher {
                     sum+=score;
                     square_sum+= score*score;
                     sum_case_vide += terrain.nbCaseVide(terrain.getTerrain());
+                    sum_ratio_aire += (score*100)/(terrain.getLarg()*terrain.getProf());
                     terrain.terrainVide();
                 }
                 break;
@@ -50,6 +54,7 @@ public class Launcher {
                     sum+=score;
                     square_sum+= score*score;
                     sum_case_vide += terrain.nbCaseVide(terrain.getTerrain());
+                    sum_ratio_aire += (score*100)/(terrain.getLarg()*terrain.getProf());
                     terrain.terrainVide();
                 }
                 break;
@@ -61,6 +66,7 @@ public class Launcher {
                     sum+=score;
                     square_sum+= score*score;
                     sum_case_vide += terrain.nbCaseVide(terrain.getTerrain());
+                    sum_ratio_aire += (score*100)/(terrain.getLarg()*terrain.getProf());
                     terrain.terrainVide();
                 }
                 break;
@@ -72,6 +78,7 @@ public class Launcher {
                     sum+=score;
                     square_sum+= score*score;
                     sum_case_vide += terrain.nbCaseVide(terrain.getTerrain());
+                    sum_ratio_aire += (score*100)/(terrain.getLarg()*terrain.getProf());
                     terrain.terrainVide();
                 }
                 break;
@@ -83,8 +90,9 @@ public class Launcher {
         double moyenne = sum / nb_rep;
         double ecart_type = Math.sqrt(square_sum / nb_rep - moyenne * moyenne); //cf. Wikipedia
         double case_vide = sum_case_vide / nb_rep;
+        double ratio_aire = sum_ratio_aire / nb_rep;
         System.out.println("sum is "+sum);
         System.out.println("Aprés "+nb_rep+" répétitions, voici les statistiques:");
-        System.out.println("Moyenne : "+moyenne+"\nÉcart-type : "+ecart_type + "\nMoyenne case vide : " + case_vide);
+        System.out.println("Moyenne : "+moyenne+"\nÉcart-type : "+ecart_type + "\nMoyenne case vide : " + case_vide + "\nMoyenne ratio aire : " + ratio_aire);
     }
 }
